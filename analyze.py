6e6d89ae178a8a5e8f84a6ce30f9f0d72c255cfa
annieqin <annie__qin@163.com>
2016-05-08 10:53:17 +0800

    分析工资与工作年限的关系

--- analyze.py
+++ analyze.py
@@ -3,6 +3,7 @@
 __author__ = 'AnnieQin <annie__qin@163.com>'
 
 import re
+from collections import defaultdict
 from model import Job
 import datetime
 import matplotlib.pyplot as plt
@@ -28,9 +29,88 @@ def main():
     cities = ['北京', '上海', '广州', '深圳', '杭州']
     # cities = ['北京']
 
-    positionnum_city(positions, cities)
+    # positionnum_city(positions, cities)
+    # salary_city(positions, cities)
+    # companysize_city(positions, cities)
+    salary_workyear(positions)
 
-    salary_city(positions, cities)
+
+def salary_workyear(positions):
+    salaries = {}
+    work_year = set()
+    for position in positions:
+        salaries_position = defaultdict(list)
+        p = positions[position].select(Job.work_year,
+                                       Job.salary,
+                                       Job.position_name)
+        for i in p:
+            re_salary = re.findall(r'\d{1,2}', i.salary)
+            if re_salary:
+                if len(re_salary) == 2:
+                    re_salary = (int(re_salary[0]) + int(re_salary[1])) / 2
+                else:
+                    re_salary = int(re_salary[0])
+            re_workyear = re.search(r'\d{1,2}-\d{1,2}|\d{1,2}', i.work_year)
+            if re_workyear:
+                salaries_position[re_workyear.group()].append(re_salary)
+                work_year.add(re_workyear.group())
+            else:
+                salaries_position[i.work_year].append(re_salary)
+                work_year.add(i.work_year)
+        salaries[position] = salaries_position
+    work_year = [u'\u4e0d\u9650', u'\u5e94\u5c4a\u6bd5\u4e1a\u751f', '1', '1-3', '3-5', '5-10', '10']
+
+    for p in salaries:
+        for wy in salaries[p]:
+            salaries[p][wy] = np.mean(np.array(salaries[p][wy]))
+
+    x = [i+1 for i in range(len(work_year))]
+
+    ys = {}
+    for p in salaries:
+        y = []
+        for w in work_year:
+            if salaries[p][w]:
+                y.append(salaries[p][w])
+            else:
+                y.append(0)
+        ys[p] = y
+
+    draw(x, ys, 'NoLimit Graduates <1       1-3      3-5      5-10      >10',
+         'Salary (k)', 'Salary - Work Year')
+
+
+def companysize_city(positions, cities):
+    companysize = {}
+    for position in positions:
+        c_city = {}
+        for city in cities:
+            c_city[city] = []
+            c = positions[position].where(Job.city == city)
+            for i in c:
+                re_size = re.findall(r'\d{1,5}', i.company_size)
+                if re_size:
+                    if len(re_size) == 2:
+                        re_size = (int(re_size[0]) + int(re_size[1])) / 2
+                    else:
+                        re_size = int(re_size[0])
+                c_city[city].append(re_size)
+            c_city[city] = np.mean(np.array(c_city[city]))
+        companysize[position] = c_city
+
+    x = [i+1 for i in range(len(cities))]
+    ys = {}
+    for position in positions:
+        y = []
+        for city in cities:
+            y.append(companysize[position][city])
+        ys[position] = y
+
+    x_cities = ''
+    for i in cities:
+        x_cities += chinese_to_pinyin(i)+str('    ')
+
+    draw(x, ys, x_cities, 'Company Size', 'Company Size - City')
 
 
 def salary_city(positions, cities):
